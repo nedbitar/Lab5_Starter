71015f14733dce1fe2c742711be5517907b00a0f
nedbitar <nabitar@ucsd.edu>
2024-05-07 17:05:24 -0700

    Unit Tests

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Lab 5 - Starter
 Ned Bitar
+
+1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+I would use unit tests to make sure that the messages sent are being exactly the same as the messages recieved. You can also write tests to see how the program will react with special characters and no text.
+
+2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+I would use unit tests to make sure the user cannot exceed the specified limit. I would test it in several languages to see how my code would function in those various languages.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,79 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+describe('Test Functions', () => {
+  describe('isPhoneNumber', () => {
+    test('Returns true for valid phone number format', () => {
+      expect(isPhoneNumber('123-123-1234')).toBe(true);  
+    });
+    test('Returns true for valid phone number format', () => {
+      expect(isPhoneNumber('000-000-0000')).toBe(true);  
+    });
+    test('Returns false for invalid phone number format', () => {
+      expect(isPhoneNumber('--')).toBe(false);  
+    });
+    test('Returns false for invalid phone number format', () => {
+      expect(isPhoneNumber('abc-efg-hijk')).toBe(false);  
+    });
+  });
+
+  describe('isEmail', () => {
+    test('Returns true for valid email format', () => {
+      expect(isEmail('nabitar@ucsd.edu')).toBe(true);  
+    });
+    test('Returns true for valid email format', () => {
+      expect(isEmail('nabitar1@ucsd.edu')).toBe(true);  
+    });
+    test('Returns false for invalid email format', () => {
+      expect(isEmail('nabitar@ucsd')).toBe(false);  
+    });
+    test('Returns false for invalid email format', () => {
+      expect(isEmail('nabitareng.ucsd.edu')).toBe(false);  
+    });
+  });
+
+  describe('isStrongPassword', () => {
+    test('Returns true for strong password format', () => {
+      expect(isStrongPassword('HelloWorld123_')).toBe(true);  
+    });
+    test('Returns true for strong password format', () => {
+      expect(isStrongPassword('Ned1')).toBe(true);  
+    });
+    test('Returns false for weak password format', () => {
+      expect(isStrongPassword('nabitar@ucsd')).toBe(false);  
+    });
+    test('Returns false for weak password format', () => {
+      expect(isStrongPassword('123Pass_')).toBe(false);  
+    });
+  });
+
+  describe('isDate', () => {
+    test('Returns true for valid date format', () => {
+      expect(isDate('12/31/2026')).toBe(true);  
+    });
+    test('Returns true for valid date format', () => {
+      expect(isDate('5/7/2026')).toBe(true);  
+    });
+    test('Returns false for invalid date format', () => {
+      expect(isDate('2026/12/31')).toBe(false);  
+    });
+    test('Returns false for invalid date format', () => {
+      expect(isDate('31/12/26')).toBe(false);  
+    });
+  });
+
+  describe('isHexColor', () => {
+    test('Returns true for valid orange hex color', () => {
+      expect(isHexColor('#FFA500')).toBe(true);  
+    });
+    test('Returns true for valid lower-case orange hex color', () => {
+      expect(isHexColor('#ffa500')).toBe(true);  
+    });
+    test('Returns false for invalid hex code with missing character', () => {
+      expect(isHexColor('#FFA50')).toBe(false);  
+    });
+    test('Returns false for invalid hex code with invalid character', () => {
+      expect(isHexColor('#FFA50G')).toBe(false);  
+    });
+  });
+});
